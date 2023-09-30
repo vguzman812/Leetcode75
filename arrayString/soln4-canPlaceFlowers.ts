15d6c467a157df79f7aadeba22e38a36178fe2c1
vincent <100250184+vguzman812@users.noreply.github.com>
2023-09-30 12:19:22 -0400

    redo solution 4

--- arrayString/soln4-canPlaceFlowers.ts
+++ arrayString/soln4-canPlaceFlowers.ts
@@ -1,14 +1,48 @@
-// https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+/**
+ * https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * Given integer array, flowerbed containing 0s and 1s
+ * 0 === empty
+ * 1 === occupied
+ * given integer n
+ * return boolean
+ * true if n flowers can be planted without violating adjacency
+ * false otherwise
+ * 0 <= n <= flowerbed.length
+ * 1 <= flowerbed.length <= 2 * 104
+ *
+ * @example
+ * Input: flowerbed = [1,0,0,0,1], n = 1
+ * Output: true
+ *
+ * Input: flowerbed = [1,0,0,0,1], n = 2
+ * Output: false
+ *
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @returns {boolean}
+ *
+ * @pseudocode
+ *  seedsToPlant = n
+ *  iterate through flowerbed
+ *      if current bed is empty and !flowerbed i-1 and !flowebed i+1
+ *          then plant a seed
+ *          decrement seeds to plant
+ *  return seeds to plant <= 0
+ *
+ */
 
-function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-    let numAdjacentFree: number = 0;
-    flowerbed.forEach((_, index) => {
-        const prev: number = index-1;
-        const next: number = index+1;
-        if (!flowerbed[prev] && !flowerbed[index] && !flowerbed[next]){
-            numAdjacentFree++;
-            flowerbed[index] = 1
-        }
-    })
-    return numAdjacentFree >= n
-}
+var canPlaceFlowers = function (flowerbed, n) {
+	let seedsToPlant = n;
+	for (let i = 0; i < flowerbed.length; i++) {
+		if (seedsToPlant === 0) break;
+		let currentPlot = flowerbed[i];
+		let nextPlot = flowerbed[i + 1];
+		let prevPlot = flowerbed[i - 1];
+		if (!currentPlot && !nextPlot && !prevPlot) {
+            flowerbed[i] = 1;
+			seedsToPlant--;
+		}
+	}
+    return seedsToPlant <= 0
+};

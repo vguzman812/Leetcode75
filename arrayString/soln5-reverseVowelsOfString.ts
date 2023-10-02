79e272c272195ef34048d3a2789781d593508421
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-02 14:52:48 -0400

    Add solution 6

--- arrayString/soln5-reverseVowelsOfString.ts
+++ arrayString/soln5-reverseVowelsOfString.ts
@@ -1,23 +1,23 @@
 /**
  * https://leetcode.com/problems/reverse-vowels-of-a-string/?envType=study-plan-v2&envId=leetcode-75
- * 
+ *
  * @description
  * we ar given string s
  * reverse vowels but leave other letters alone
  * return reversed vowel word
  * vowels can appear upper or lower any # of times
  * 1 <= s.length <= 3 * 10^5
- * 
+ *
  * @example
  * Input: s = "hello"
  * Output: "holle"
- * 
+ *
  * Input: s = "leetcode"
  * Output: "leotcede"
- * 
+ *
  * @param {string} s
  * @returns {string}
- * 
+ *
  * @pseudocode
  *  create array with lowercase vowels
  *  create empty array to hold vowels
@@ -28,27 +28,25 @@
  *      if vowels includes current character
  *          s[i] = vowel array.pop
  *  return s
- * 
+ *
  */
 
 var reverseVowels = function (s: string) {
-    let vowels: string[] = ["a", "e", "i", "o", "u"];
-    let reversedVowels: string[] = [];
-    let newString: string[] = s.split("")
-    for (let i = 0; i < s.length; i++) {
-      if (vowels.includes(s[i].toLowerCase())) {
-        reversedVowels.unshift(s[i])
-      }
-    }
-    console.log(reversedVowels)
-    for (let i = 0; i < s.length; i++){
-    if (vowels.includes(s[i].toLowerCase())) {
-      const vowel = reversedVowels.shift()
-      if (vowel !== undefined) newString[i] = vowel
-    }
-  }
-  console.log(newString.join(""))
-  return newString.join("")
-  };
-  
-  reverseVowels("hello")
+	let vowels: string[] = ["a", "e", "i", "o", "u"];
+	let reversedVowels: string[] = [];
+	let newString: string[] = s.split("");
+	for (let i = 0; i < s.length; i++) {
+		if (vowels.includes(s[i].toLowerCase())) {
+			reversedVowels.unshift(s[i]);
+		}
+	}
+	for (let i = 0; i < s.length; i++) {
+		if (vowels.includes(s[i].toLowerCase())) {
+			const vowel = reversedVowels.shift();
+			if (vowel !== undefined) newString[i] = vowel;
+		}
+	}
+	return newString.join("");
+};
+
+reverseVowels("hello");
--- arrayString/soln6-reverseWordsInString.ts
+++ arrayString/soln6-reverseWordsInString.ts
@@ -1,7 +1,40 @@
-// https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
+/**
+ * https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * we are given an input string s
+ * reverse the order of the words.
+ * keep the letters in order.
+ * words will be separated by 1 or more spaces
+ * return reversed words with one space in between
+ * may contain leading or trailing spaces
+ * 
+ * @param {string} s
+ * @returns {string}
+ * 
+ * @example
+ * Input: s = "the sky is blue"
+ * Output: "blue is sky the"
+ * 
+ * Input: s = "  hello world  "
+ * Output: "world hello"
+ * 
+ * Input: s = "a good   example"
+ * Output: "example good a"
+ * 
+ * @pseudocode
+ *  trim string.
+ *  split string by space
+ *  filter out empty array elements
+ *  reverse array
+ *  join array
+ *  see whats up
+ */
 
 function reverseWords(s: string): string {
-    let arr: string[] = s.trim().split(" ");
-    arr = arr.filter(element => element !== undefined);
-    return arr.join(" ")
-};
+    let trimmed = s.trim()
+    let arr = trimmed.split(" ")
+    let trimmedArr = arr.filter(e => e)
+    let reversedArr = trimmedArr.reverse()
+    let joinedString = reversedArr.join(" ")
+    return joinedString
+}

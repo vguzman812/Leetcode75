5655be14bd8905e14323395a45ce9219339b23f0
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-12 15:52:26 -0400

    redo 13

--- twoPointers/soln13-maxNumKSumPairs.ts
+++ twoPointers/soln13-maxNumKSumPairs.ts
@@ -1,33 +1,55 @@
-// https://leetcode.com/problems/max-number-of-k-sum-pairs/?envType=study-plan-v2&envId=leetcode-75
-function maxOperations(a: number[], k: number): number {
-    let map = new Map<number, number>();
-    let cnt = 0;
-	
-	// filling the map
-    for(let i of a) {
-        map.set(i, (map.get(i)??0) + 1);
-    }
-    
-    for(let n of a) {
-        let nCnt = map.get(n); 
-        
-		// if n is not present in map continue traversal choose next n (if any)
-		if(!nCnt) continue; 
-		
-		// if n is present choose it and deduct the count
-        map.set(n, nCnt-1);
-		
-		// get the count of complement
-        let nCnt1 = map.get(k - n);
-		
-		// if count of n and n complement exist then its a hit increase count
-        if (nCnt && nCnt1) {
-            map.set(k - n, nCnt1 - 1);
-            cnt++;
-        } else {
-			// no complement exist so release the choosen n by setting count of n
-            map.set(n, nCnt);
-        }
-    }
-    return cnt;
-};
+/**
+ * https://leetcode.com/problems/max-number-of-k-sum-pairs/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * given integer array nums
+ * given integer k
+ * pick two numbers from array whose sum === k
+ * remove those two numbers from array
+ * return max number of operations performable on array
+ *
+ * @param {number[]} nums
+ * @param {number} k
+ * @returns {number}
+ *
+ * @example
+ * Input: nums = [1,2,3,4], k = 5
+ * Output: 2
+ * Explanation: Starting with nums = [1,2,3,4]:
+ * - Remove numbers 1 and 4, then nums = [2,3]
+ * - Remove numbers 2 and 3, then nums = []
+ * There are no more pairs that sum up to 5, hence a total of 2 operations.
+ *
+ * Input: nums = [3,1,3,4,3], k = 6
+ * Output: 1
+ * Explanation: Starting with nums = [3,1,3,4,3]:
+ * - Remove the first two 3's, then nums = [1,4,3]
+ * There are no more pairs that sum up to 6, hence a total of 1 operation.
+ *
+ * @pseudocode
+ *  1. Initialize a hash table and set count = 0.
+ *  2. Loop through each number x in nums.
+ *      - If (k - x) exists in the hash table:
+ *          - Increment count by 1.
+ *          - Decrement the value of (k - x) in the hash table by 1.
+ *          - If the value of (k - x) becomes 0, remove it from the hash table.
+ *      - Else:
+ *          - Increment the value of x in the hash table by 1.
+ *  3. Return count.
+ *
+ */
+
+function maxOperations(nums: number[], k: number): number {
+	let hash = new Map();
+	let count = 0;
+	for (let num of nums) {
+		const complement = k - num;
+
+		if (hash.get(complement) > 0) {
+			count++;
+			hash.set(complement, hash.get(complement) - 1);
+		} else {
+			hash.set(num, (hash.get(num) || 0) + 1);
+		}
+	}
+    return count
+}

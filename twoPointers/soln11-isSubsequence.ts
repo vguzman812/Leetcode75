68b1d6d34bcbaa06482916275aafbbd05557898c
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-12 15:24:33 -0400

    redo 11 12

--- twoPointers/soln11-isSubsequence.ts
+++ twoPointers/soln11-isSubsequence.ts
@@ -1,37 +1,59 @@
-// https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
+/**
+ * https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * given two stirngs s and t
+ * return bool
+ * s is subsequence of t?
+ * if you can delete stuff in t and make s then s is subsequence of t
+ * probably just check if s comes in the same order in t then yes.
+ *
+ * @example
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ *
+ * Input: s = "axc", t = "ahbgdc"
+ * Output: false
+ *
+ * @param {string} s
+ * @param {string} t
+ * @returns {boolean}
+ *
+ * @pseudocode
+ *  function isSubsequence(s: string, t: string): boolean {
+ *    // Initialize s_pointer and t_pointer to 0
+ *    let s_pointer = 0;
+ *    let t_pointer = 0;
+ *    // Iterate through string t
+ *    while (t_pointer < t.length) {
+ *      // If s_pointer exceeds s.length, that means all characters of s were found in t
+ *      if (s_pointer === s.length) {
+ *        return true;
+ *      }
+ *      // Compare current character of s with current character of t
+ *      if (s[s_pointer] === t[t_pointer]) {
+ *        // Move s_pointer to next character in s
+ *        s_pointer++;
+ *      }
+ *      // Always move t_pointer to the next character in t
+ *      t_pointer++;
+ *    }
+ *    // After iterating through t, if s_pointer has reached the end of s, then s is a subsequence of t
+ *    return s_pointer === s.length;
+ *  }
+ *
+ */
+
 function isSubsequence(s: string, t: string): boolean {
-    let pointerS: number = 0;
-    let pointerT: number = 0;
-    while (pointerT < t.length){
-        if(t[pointerT] === s[pointerS]){
-            pointerS++;
-        }
-        pointerT ++;
-        if (pointerS === s.length){
-            return true;
-        }
-    }
-    return false
-};
-/*
-time complexity is 2n
-space complexity is 1
-function isSubsequence(s, t)
-    initialize pointerS to 0
-    initialize pointerT to 0
-
-    while pointerT is less than length of t
-        if character at pointerT in t equals character at pointerS in s
-            increment pointerS by 1
-        end if
-
-        increment pointerT by 1
-
-        if pointerS equals length of s
-            return true
-        end if
-    end while
-
-    return false
-end function
-*/
+	let s_pointer = 0;
+	let t_pointer = 0;
+	while (t_pointer < t.length) {
+		if (s_pointer === s.length) {
+			return true;
+		}
+		if (s[s_pointer] === t[t_pointer]) {
+			s_pointer++;
+		}
+		t_pointer++;
+	}
+	return s_pointer === s.length;
+}
--- twoPointers/soln12-containerWithMostWater.ts
+++ twoPointers/soln12-containerWithMostWater.ts
@@ -1,25 +1,43 @@
-// https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
+/**
+ * https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * given integer array height length n
+ * n vertical lines drawn such that two endpoints of ith line are (i, 0) and (i, height[i])
+ * find two lines that form a container with the biggest area
+ *
+ * @example
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ * @param {number[]} height
+ * @returns {number} area
+ * @pseudocode
+ * maxArea is 0 initially
+ * leftIndex side 0
+ * rightIndex side  = height array length - 1
+ * while left pointer less than or equal to right pointer
+ *      let currentArea = (width aka right - left) * smallest height so Math.min(height[left], height[right])
+ *      max = max value either currentArea or max
+ *      change index value for either left or right
+ *      if left value is smaller increment index
+ *      if right value is smaller decrement index
+ * return max
+ */
+
 function maxArea(height: number[]): number {
-    let max: number = 0;
-    let left: number = 0;
-    let right:number = height.length -1
-    while (left <= right){
-        let currentArea:number = Math.min(height[left], height[right]) * (right - left);
-        max = Math.max(max, currentArea);
-        if (height[left] < height[right]){
-            left ++
-        }
-        else right --
-    }
-    return max
-};
-
-// find highest height and record which index it's at and height.
-// look at next index. if current index > highest height, 
-//      highest height == current index value
-//      second highest height == last highest height index value.
-// else if current index value > second highest height
-//      second highest height = current index value
-// width == highest index of the two heights - lowest index of the two heights.
-// area = second highest height * width
-
+	let maxArea = 0;
+	let leftIndex = 0;
+	let rightIndex = height.length - 1;
+	while (leftIndex <= rightIndex) {
+		let currentWidth = rightIndex - leftIndex;
+		let currentHeight = Math.min(height[leftIndex], height[rightIndex]);
+		let currentArea = currentWidth * currentHeight;
+		maxArea = Math.max(maxArea, currentArea);
+		height[leftIndex] < height[rightIndex] ? leftIndex++ : rightIndex--;
+	}
+	return maxArea;
+}

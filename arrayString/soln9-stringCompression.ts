1b61e5b0ea340a14d4bfcfce092a071455fc1963
vincent <100250184+vguzman812@users.noreply.github.com>
2023-10-12 10:29:38 -0400

    redo 9

--- arrayString/soln9-stringCompression.ts
+++ arrayString/soln9-stringCompression.ts
@@ -1,42 +1,89 @@
-// https://leetcode.com/problems/string-compression/?envType=study-plan-v2&envId=leetcode-75
-function compress(chars) {
-    let write = 0; // Pointer to keep track of where to write compressed characters
-    let read = 0; // Pointer to iterate through the array
-  
-    while (read < chars.length) {
-      let char = chars[read];
-      let count = 0;
-  
-      // Count consecutive repeating characters
-      while (read < chars.length && chars[read] === char) {
-        read++;
-        count++;
-      }
-  
-      // Write the character
-      chars[write++] = char;
-  
-      // If the group's length is greater than 1, write the length
-      if (count > 1) {
-        let countStr = count.toString();
-        for (let i = 0; i < countStr.length; i++) {
-          chars[write++] = countStr[i];
-        }
-      }
-    }
-}
+/**
+ * https://leetcode.com/problems/string-compression/?envType=study-plan-v2&envId=leetcode-75
+ * @description
+ * given integer array chars
+ * begin with empty string s
+ * for eahc group of repeating chars in chars
+ * if group length is 1
+ *    add to s
+ * if group length > 1
+ *    add char to s and add #of repeats to s
+ * s should not be returned separately
+ * s should be stored in input array chars
+ * repeat lengths of more than 1 digit should be separated so 27 = 2 7
+ * can only use On memory
+ * return length of new arrary
+ *
+ * @example
+ * Input: chars = ["a","a","b","b","c","c","c"]
+ * Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+ * Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3"
+ *
+ * Input: chars = ["a"]
+ * Output: Return 1, and the first character of the input array should be: ["a"]
+ * Explanation: The only group is "a", which remains uncompressed since it's a single character.
+ *
+ * Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+ * Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+ * Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+ *
+ *
+ * @param {string[]} chars
+ * @returns {number} chars.length
+ *
+ * @pseudocode
+ *  function stringCompression(chars: string[]): number{
+ *    let writePointer = 0; // Pointer to indicate where we need to write the compressed string
+ *    let readPointer = 0;  // Pointer to read the original string
+ *    while (readPointer < chars.length) {
+ *      let char = chars[readPointer];
+ *      let count = 0;
+ *      // Count the consecutive occurrences of char
+ *      while (readPointer < chars.length && chars[readPointer] === char) {
+ *        readPointer++;
+ *        count++;
+ *      }
+ *      // Write the character to the array
+ *      chars[writePointer] = char;
+ *      writePointer++;
+ *      // If there is more than one occurrence, write the count after the character
+ *      if (count > 1) {
+ *        let countStr = count.toString(); // convert the count to string to handle multiple digits
+ *        for (let digit of countStr) {
+ *          chars[writePointer] = digit;
+ *          writePointer++;
+ *        }
+ *      }
+ *    }
+ *    // Return the length of the compressed array
+ *    return writePointer;
+ *  }
+ *
+ */
 
-/*
-Initialize Pointers: Set write pointer to 0 and read pointer to 0.
-Start Iteration: Begin a loop that continues while read is less than the length of the array.
-a. Identify Character: Store the character at the read position as char.
-b. Initialize Count: Set a count variable to 0.
-c. Count Consecutive Characters: Begin a loop that continues while read is less than the length of the array and the character at read is equal to char.
-Increment read.
-Increment count.
-d. Write Character: Write char to the write position in the array, and increment write.
-e. Write Count if Greater Than 1: If count is greater than 1:
-Convert count to a string.
-Iterate through each digit of the string, writing it to the write position in the array, and incrementing write.
-Return New Length: Return the value of the write pointer, representing the new length of the array.
-*/
+function compress(chars: string[]): number {
+	let i = chars.length - 1;
+	while (i >= 0) {
+		const char = chars[i];
+		let duplicateCount = 0;
+
+		// move i left until no more duplicates
+		while (i >= 0 && char === chars[i]) {
+			duplicateCount += 1;
+			i -= 1;
+		}
+		// if multiple duplicates
+		if (duplicateCount > 1) {
+			// delete characters after the first repeating char
+			// it's i + 2 since i is before the repeating chars, and
+			// the second repeating char needs replaced
+
+			// replace with amount of duplicates
+			// split digits into individual characacters
+			// for example, 12 becomes "1", "2"
+			chars.splice(i + 2, duplicateCount - 1, ...String(duplicateCount));
+		}
+	}
+
+	return chars.length;
+}
